18acd1ec05738e068c876e224ea896a2a8e3cb81
Jallvar <kllaid63@yandex.ru>
2021-03-27 22:25:54 +0400

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 return [
     "tgAdminID" => 409847995,
-    "tgKey" => "3247499034:AAHwfSSjOlHQC5qygk1iDmpzk1CBEdpi_0A",
+    "tgKey" => "",
     "tgTypeUpdate" => \Jallvar\Core\Bot\UpdateTypes::$MANUAL,
     "groupID" => -162172379,
-    "vkApiKey" => "3227d13c9b8347975fec077c6a45db4997521485f86c2dea67bc1a3208aa44c72d83d3c82e9546987050b",
+    "vkApiKey" => "",
 ];
